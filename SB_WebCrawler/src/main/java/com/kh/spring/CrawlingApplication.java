d966c163ecb3664f5de3d1339df61f8163d5b9c0
nobodj <dongxuan09@naver.com>
2018-07-06 09:35:48 +0900

    springboot에서 프로젝트내부의 phantomjs.exe위치 상대경로로 지정

--- SB_WebCrawler/src/main/java/com/kh/spring/CrawlingApplication.java
+++ SB_WebCrawler/src/main/java/com/kh/spring/CrawlingApplication.java
@@ -7,8 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CrawlingApplication {
 
 	public static void main(String[] args) {
-		//HeadlessCrawler : phantomjs실행파일의 경로를 시스템 속성으로 설정해서 ghostDriver에서 참조하도록 한다. 
-		System.setProperty("phantomjs.binary.path", "C:\\dev\\phantomjs-2.1.1-windows\\bin\\phantomjs.exe");
+		//HeadlessCrawler : 본인컴퓨터에 다운로드 받은 phantomjs실행파일의 경로를 시스템 속성으로 설정해서 ghostDriver에서 참조하도록 한다. 
+		//System.setProperty("phantomjs.binary.path", "C:\\dev\\phantomjs-2.1.1-windows\\bin\\phantomjs.exe");
+		//상대주소로 입력시
+		System.setProperty("phantomjs.binary.path", "phantomjs-2.1.1-windows/bin/phantomjs.exe");
 		
 		SpringApplication.run(CrawlingApplication.class, args);
 	}
